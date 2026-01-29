fe2ad84c0d07502f9a0c1da7e469b0999b23ea2e
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-01-29 14:21:58 -0800

    Homepage triangles

--- components/HomepageWelcome/HomepageWelcome.js
+++ components/HomepageWelcome/HomepageWelcome.js
@@ -3,8 +3,12 @@ import styles from './HomepageWelcome.module.scss';
 export default function HomepageWelcome() {
   return (
     <div className={styles.homepageWelcome}>
-      <h2 className="intro">Welcome</h2>
-      <p className="intro">Cal Poly Print & Copy is your print solutions center. Conveniently located in the Buidling 35, we specialize in small and large format prints for presentations, events, and the classroom. With easy online ordering and flexible hours, Cal Poly Print & Copy is the premier solution for all of your printing needs.</p>
+      <div className={styles.panel}>
+        <div className={styles.content}>
+          <h2 className="intro">Welcome</h2>
+          <p className="intro">Cal Poly Print & Copy is your print solutions center. Conveniently located in the Buidling 35, we specialize in small and large format prints for presentations, events, and the classroom. With easy online ordering and flexible hours, Cal Poly Print & Copy is the premier solution for all of your printing needs.</p>
+        </div>
+      </div>
     </div>
   );
 }
